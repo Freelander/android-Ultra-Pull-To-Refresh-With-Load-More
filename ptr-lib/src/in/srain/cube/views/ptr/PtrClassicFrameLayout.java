6d836feb526bbdd248b3a1e38dd6d99982798bcc
Freelander <huanggaojun13@gmail.com>
2016-03-23 16:58:04 +0100

    change progress style

--- ptr-lib/src/in/srain/cube/views/ptr/PtrClassicFrameLayout.java
+++ ptr-lib/src/in/srain/cube/views/ptr/PtrClassicFrameLayout.java
@@ -2,25 +2,58 @@ package in.srain.cube.views.ptr;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 
 public class PtrClassicFrameLayout extends PtrFrameLayout {
 
     private PtrClassicDefaultHeader mPtrClassicHeader;
     private PtrClassicDefaultFooter mPtrClassicFooter;
 
+    private int mTouchSlop;
+    private float mPrevX;
+
     public PtrClassicFrameLayout(Context context) {
         super(context);
         initViews();
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     public PtrClassicFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         initViews();
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     public PtrClassicFrameLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         initViews();
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    /**
+     * 解决图片轮播与下拉刷新的滑动事件冲突
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPrevX = MotionEvent.obtain(event).getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                final float eventX = event.getX();
+                float xDiff = Math.abs(eventX - mPrevX);
+
+                if (xDiff > mTouchSlop) {
+                    return false;
+                }
+        }
+
+        return super.onInterceptTouchEvent(event);
     }
 
     private void initViews() {
